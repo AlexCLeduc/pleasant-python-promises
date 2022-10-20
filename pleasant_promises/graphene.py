c9dafab9f30b026f0a21eaa5ae2c323df5f05fe0
AlexCLeduc <ledualex@gmail.com>
2022-10-20 17:42:45 -0400

    fix case where synchronous parent has generator children?

--- pleasant_promises/graphene.py
+++ pleasant_promises/graphene.py
@@ -1,12 +1,17 @@
-import graphene
 from inspect import isgenerator
 
+import graphene
 from promise import is_thenable
 
 from .core import promise_from_generator
 
+
 def promised_generator_middleware(next, root, info, **kwargs):
     next_val = next(root, info, **kwargs)
+
+    if isgenerator(next_val):
+        prom = promise_from_generator(next_val)
+        next_val = prom
 
     if not is_thenable(next_val):
         return next_val
